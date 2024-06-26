1d77c0618a65cde1356d2db58fe74dac2e1ecdbf
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-06-26 22:38:16 +0200

    add livecoding/project/

--- livecoding/language/coding.cmake
+++ livecoding/language/coding.cmake
@@ -8,6 +8,13 @@
 #   message("${file}")
 # endforeach()
 
-set(abspath "/a/b/c.cpp")
-cmake_path(GET abspath FILENAME filename_part)
-message("${filename_part}")
+# set(abspath "/a/b/c.cpp")
+# cmake_path(GET abspath FILENAME filename_part)
+# message("${filename_part}")
+
+set(input_dir "${INPUT_DIR}")
+
+file(GLOB_RECURSE files RELATIVE ${input_dir} "*")
+message("${files}")
+
+#GLOB <variable> [LIST_DIRECTORIES true|false] [RELATIVE <path>] [CONFIGURE_DEPENDS] <globbing-expressions>...)¶
